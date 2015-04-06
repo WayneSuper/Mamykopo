4ee8d8d8add4709e1d0a92f01623030ea3e1a2ea
WayneSuper <18670937083@163.com>
2015-04-06 22:43:46 +0800

    建立个人中心，修改图片加载方式

--- app/src/main/java/cn/wayne/mamypoko/mode/home/fragment/HomeFragment.java
+++ app/src/main/java/cn/wayne/mamypoko/mode/home/fragment/HomeFragment.java
@@ -1,9 +1,10 @@
-package cn.wayne.mamypoko.modules.home.fragment;
+package cn.wayne.mamypoko.mode.home.fragment;
 
 
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.JsonReader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +13,26 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.listener.FindListener;
 import cn.wayne.mamypoko.R;
-import cn.wayne.mamypoko.modules.home.adapter.FindAdapter;
-import cn.wayne.mamypoko.modules.home.entity.FindModel;
+import cn.wayne.mamypoko.mode.home.adapter.FindAdapter;
+import cn.wayne.mamypoko.mode.home.entity.Advert;
+import cn.wayne.mamypoko.mode.home.entity.FindModel;
+import cn.wayne.mamypoko.net.MamyClient;
 import cn.wayne.mamypoko.widget.CarouselView;
 import cn.wayne.mamypoko.widget.NoScrollListView;
 import cn.wayne.mamypoko.widget.ObservableScrollView;
@@ -72,13 +85,51 @@ public class HomeFragment extends Fragment implements ObservableScrollView.OnScr
         actionsMenu.attachToScrollView(scrollView,null);
 
         mCarouselView =(CarouselView)view.findViewById(R.id.carouseView);
+        getAdverts();
+    }
+
+    private void getAdverts() {
         imageUrls=new ArrayList<String>();
         imageUrls.add("http://www.qipaox.com/tupian/200810/20081051924582.jpg");
         imageUrls.add("http://www.bz55.com/uploads1/allimg/120312/1_120312100435_8.jpg");
         imageUrls.add("http://img3.iqilu.com/data/attachment/forum/201308/21/192654ai88zf6zaa60zddo.jpg");
         imageUrls.add("http://img2.pconline.com.cn/pconline/0706/19/1038447_34.jpg");
-                imageUrls.add("http://www.kole8.com/desktop/desk_file-11/2/2/2012/11/2012113013552959.jpg");
+        imageUrls.add("http://www.kole8.com/desktop/desk_file-11/2/2/2012/11/2012113013552959.jpg");
         imageUrls.add("http://www.237.cc/uploads/pcline/712_0_1680x1050.jpg");
+        //http://www.qubaobei.com/ios/api/adr_ad.php?is_new=1&site=61&uid=4325690&user_type=2&baby_time=2015-04-01
+        RequestParams params = new RequestParams();
+        params.put("is_new","1");
+        params.put("site","61");
+        params.put("uid","4325690");
+        params.put("user_type","2");
+        params.put("baby_time","2015-04-01");
+        MamyClient.get("ios/api/adr_ad.php?",params,new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Gson gson = new Gson();
+                JsonReader reader = new JsonReader(new InputStreamReader(new ByteArrayInputStream(responseBody)));
+                reader.setLenient(true);
+//                Type type = new  <Advert>.getType();
+//               List<Advert> advert =  gson.fromJson(reader,type);
+//                List<Advert.DataEntity> adverts = (List<Advert.DataEntity>) advert.getList();
+//                imageUrls=new ArrayList<String>();
+//                for (int i = 0; i < adverts.size(); i++) {
+//                    Advert.DataEntity item = adverts.get(i);
+//                    imageUrls.add(item.getPic());
+//                }
+//                mCarouselView.setImageUris(imageUrls);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+
+
+
+
+
         mCarouselView.setImageUris(imageUrls);
     }
 
